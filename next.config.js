27cfbfcb9e25a83a2946a0e9d8dc5a543615b397
SonSpy <sonvishalspy@gmail.com>
2025-07-24 00:16:32 +0530

    config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
-  output: 'export',
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
